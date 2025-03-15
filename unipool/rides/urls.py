253037c202957c88364870c4939633b0eceb665e
Vansh Mahajan <vanshmahajan2023@gmail.com>
2025-03-15 17:15:53 +0530

    added url route for landing page

--- unipool/rides/urls.py
+++ unipool/rides/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path
 from . import views
-from.views import delete_ride
-from .views import confirm_ride
-
 
 urlpatterns = [
     path('post/', views.post_ride, name='post_ride'),
-    path('', views.ride_list, name='ride_list'),
-    path('ride/delete/<int:ride_id>/', delete_ride, name='delete_ride'),
-    path('ride/confirm/<int:ride_id>/', confirm_ride, name='confirm_ride'),
+    path('', views.landing_page, name='landing_page'),  # Make landing page as home page
+    path('ride/list/', views.ride_list, name='ride_list'),
+    path('ride/delete/<int:ride_id>/', views.delete_ride, name='delete_ride'),
+    path('ride/confirm/<int:ride_id>/', views.confirm_ride, name='confirm_ride'),
     path('rides/book/<int:ride_id>/', views.book_ride, name='book_ride'),
 ]
-
-path('rides/book/<int:ride_id>/', views.book_ride, name='book_ride'),
-
-
